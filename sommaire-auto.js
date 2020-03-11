486a72b315adb0b9eca80ee5de59fe6d836562a6
teobryer <teobryer@h001-8>
2020-03-11 16:01:45 +0100

    Exerice 2
    -Ajout du cadre autour du sommaire
    -Ajout d'un fond gris pour les éléments h1
    -Ajout d'un bouton pour afficher et cacher le sommaire

--- sommaire-auto.js
+++ sommaire-auto.js
@@ -10,7 +10,15 @@ console.log(remplirSommaire(children));
 let div = document.createElement("div");
 div.innerHTML = remplirSommaire(children);
 div.id="sommaire";
+div.style.width = "20%";
+
+div.style.border = "solid 1px black";
 document.getElementsByTagName("body")[0].insertBefore(div, document.getElementsByTagName("body")[0].firstChild);
+
+let button = document.createElement("BUTTON");
+button.innerHTML="Cacher sommaire";
+button.addEventListener("click", cacherSommaire);
+document.getElementsByTagName("body")[0].insertBefore(button, document.getElementsByTagName("body")[0].firstChild);
 
 
 function remplirSommaire(children){
@@ -26,6 +34,7 @@ function remplirSommaire(children){
 				ancrebase++;
 				sommaire+="<li id='sommaire0'><a href='#ancre-"+ancrebase+"'>"+child.innerHTML+"</a>";
 				child.id="ancre-"+ancrebase;
+				child.style.background="grey";
 				
 			
 			}
@@ -54,3 +63,23 @@ function remplirSommaire(children){
     return sommaire;
 
 }
+
+function cacherSommaire(){
+	let sommaire = document.getElementById("sommaire");
+	let button = document.getElementsByTagName("body")[0].firstChild;
+	if(sommaire.style.display=="none"){
+		sommaire.style.display="block";
+		button.innerHTML = "Cacher sommaire"
+		console.log("display : " + sommaire.style.display)
+	}
+	else {
+		sommaire.style.display="none";
+		console.log("display : " + sommaire.style.display)
+		button.innerHTML = "Afficher sommaire";
+	}
+	
+	
+
+}
+
+
